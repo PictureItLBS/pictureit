64c81b79ee62d0c6add9c73675dc3f9ef381e86c
Fabian Seluska <fabianseluska@gmail.com>
2022-04-21 09:28:30 +0200

    Added a custom logger function...

---
+++ index.js
@@ -0,0 +1,5 @@
+import getLogger from "./modules/logger.js"
+
+const log = getLogger("Server", "purple")
+
+log("Hello!", "World!")
---
+++ modules/logger.js
@@ -0,0 +1,19 @@
+import colors from 'colors'
+
+/**
+ * Takes any amount of arguments and prints them to the log...
+ * @typedef {Function} Logger
+ * @argument {string} string The string to be logged.
+ */
+
+/**
+ * This function creates logger - A function that auto-prefixes log messages.
+ * @param {string} prefix The prefix of the logger. I.e. MAIN, DATABASE, ETC.
+ * @returns {Logger}
+ */
+export default function getLogger(prefix, clr="blue") {
+    const color = colors[clr] ? clr : "blue"
+    return function(...args) {
+        console.log(colors[color](`[ ${prefix} ]`), ...args)
+    }
+}
