b3146206d0192ec1f06e6728e7bb4b16ca6697b7
Fabian Seluska <fabianseluska@gmail.com>
2022-04-27 10:08:40 +0200

    Added ground work for creating docs.

--- routes/docs.js
+++ routes/docs.js
@@ -1,7 +1,39 @@
-import { Router } from "express"
+import { Router }     from "express"
+import fs             from "fs/promises"
+import { existsSync } from "fs"
 
 const docs = Router()
 
-docs.get("/", (_, res) => res.render("docs/index.njk"))
+docs.get(
+    "/",
+    (_, res) => 
+        fs.readdir('app/views/docs/articles').then(
+            articles => res.render(
+                "docs/index.njk",
+                {
+                    articles: articles.map(article => article.replace(/\.njk$/g, ""))
+                }
+            )
+        )
+)
+docs.get(
+    "/:article",
+    (req, res) => {
+        const article = req.params.article
+
+        if (existsSync(`app/views/docs/articles/${article}.njk`))
+            return res.render(`docs/articles/${article}.njk`)
+
+        fs.readdir('app/views/docs/articles').then(
+            articles => res.render(
+                "docs/index.njk",
+                {
+                    articles: articles.map(article => article.replace(/\.njk$/g, "")),
+                    error:    `Article "${article}" does not exist!`
+                }
+            )
+        )
+    }
+)
 
 export default docs
