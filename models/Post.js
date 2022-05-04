e3fed30978661f23e4467680897a9b91e9e217db
Fabian Seluska <fabianseluska@gmail.com>
2022-05-04 08:25:16 +0200

    Minor fixes...

--- models/Post.js
+++ models/Post.js
@@ -4,19 +4,18 @@ import { ObjectId } from "mongodb"
 
 /**
  * @typedef  {object}   Post        A post on PictureIt.
- * @typedef  {object}   author      A post author.
  * @property {ObjectId} _id         The id of the post.
+ * @property {string}   author      A post author.
  * @property {string}   caption     The caption of the post.
  * @property {string}   date        The date when the post was made.
  * @property {string}   likedBy     Amount of likes on a post.
  */
 
- function Post(caption, author) {
+function Post(caption, author) {
     return {
-      caption,
-      author,
-      date: new Date(Date.now()),
-      likedBy: [],
-      
+        caption,
+        author,
+        date: new Date(Date.now()),
+        likedBy: [],
     }
-  }
+}
