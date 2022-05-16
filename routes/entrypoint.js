a6e4939e384c754477987922244dd07df3edd78d
Fabian Seluska <fabianseluska@gmail.com>
2022-05-16 14:05:03 +0200

    Beginnings of news...

--- routes/entrypoint.js
+++ routes/entrypoint.js
@@ -2,12 +2,14 @@ import { Router }   from "express"
 import apiAppRouter from "../modules/api-app-router.js"
 import auth         from "./auth.js"
 import feed         from "./feed.js"
+import news         from "./news.js"
 import profile      from "./profile.js"
 
 const routes = Router()
 routes.get("/", (_, res) => res.respond(true, "HELLO WORLD!", "pages/landing.njk", {}))
 routes.use("/auth",    auth)
 routes.use("/feed",    feed)
+routes.use("/news",    news)
 routes.use("/profile", profile)
 
 const api = Router()
---
+++ routes/news.js
@@ -0,0 +1,9 @@
+import { Router }    from "express"
+import validateToken from "../modules/validateToken.js"
+
+const news = Router()
+
+news.use(validateToken)
+news.get("/", (req, res) => res.respond(true, "FEED", "pages/news.njk", {}))
+
+export default news
