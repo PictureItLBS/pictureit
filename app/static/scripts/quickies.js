0d284b254bc28bfe5d226655741f96d1ef0a15ac
Fabian Seluska <fabianseluska@gmail.com>
2022-05-30 14:36:39 +0200

    Copy-paste of image upload from old PictureIt.

---
+++ app/static/scripts/quickies.js
@@ -0,0 +1,26 @@
+import popup from './popup.js'
+
+document.querySelector('.quickie-button#liked-posts')
+    .addEventListener('click', ev => window.location.pathname = "/app/profile/likedPosts")
+
+document.querySelector(".quickie-button#add-post")
+    .addEventListener("click", ev => {
+        ev.preventDefault()
+        popup.show("upload")
+    })
+
+document.querySelector(".upload-cancel-btn")
+    .addEventListener("click", ev => {
+        ev.preventDefault()
+        popup.hide()
+    })
+
+document.querySelector(".input-upload-image")
+    .addEventListener("change", ev => {
+        const input = ev.target
+        if (input?.files[0]) {
+            const reader = new FileReader()
+            reader.onload = (e) => document.querySelector(".upload-preview-image").style.backgroundImage = `url(${e.target.result})`
+            reader.readAsDataURL(input.files[0])
+        }
+})
