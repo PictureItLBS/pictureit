f895170963d7f457f4ce3cb0cc56da41da9aa2a0
Fabian Seluska <fabianseluska@gmail.com>
2022-06-01 09:16:53 +0200

    Begun working on user settings.

--- routes/profile.js
+++ routes/profile.js
@@ -4,6 +4,7 @@ import validateToken from "../modules/validateToken.js"
 const profile = Router()
 
 profile.use(validateToken)
-profile.get("/", (_, res) => res.respond(true, "FEED", "pages/profile.njk", { activeTab: "profile" }))
+profile.get("/",         (_, res) => res.respond(true, "FEED", "pages/profile/profile.njk",  { activeTab: "profile" }))
+profile.get("/settings", (_, res) => res.respond(true, "FEED", "pages/profile/settings.njk", { activeTab: "settings" }))
 
 export default profile
