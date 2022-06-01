06e530e3d7ef926869e79157c10bcad805998d7b
Fabian Seluska <fabianseluska@gmail.com>
2022-06-01 09:35:48 +0200

    News system beginnings...

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,17 @@
+import fs            from "fs"
 import { Router }    from "express"
 import validateToken from "../modules/validateToken.js"
 
 const news = Router()
 
 news.use(validateToken)
-news.get("/", (req, res) => res.respond(true, "FEED", "pages/news.njk", { activeTab: "news" }))
+news.get(
+    "/",
+    (req, res) => {
+        const newsFiles = fs.readdirSync("./app/views/news").reverse()
+        const news = newsFiles.map(newsFile => fs.readFileSync(`./app/views/news/${newsFile}`).toString())
+        res.respond(true, { news }, "pages/news.njk", { activeTab: "news" })
+    }
+)
 
 export default news
